7d6da19127659aa354cd3f45309da524f64da4a8
Hendra manase sitorus <72810939+Hendra89ms@users.noreply.github.com>
2023-07-27 08:24:47 +0700

    perubahan data

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -6,7 +6,7 @@ function NotFound() {
     return (
         <div className='flex justify-center items-center fixed inset-0 z-[999] bg-[#1a1b1f]'>
             <div className='text-red-500 font-semibold text-2xl'>
-                <h1>ERROR... PAGE NOT FOUND !!!</h1>
+                <h1>SORRY... WEBSITE IS UNDER DEVELOPMENT !!!</h1>
                 <Link to={'/'}>
                     <div className='text-lg text-center mt-4 text-white hover:text-red-700 duration-300 ease-in-out'>
                         Back to Home
